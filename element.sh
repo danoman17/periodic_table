2e6ec8e30a71cfd2e2c7abd27dd36439ad877e34
Daniel Flores Rodríguez <daniflores374@gmail.com>
2024-07-11 21:53:08 +0000

    feat: element output message

--- element.sh
+++ element.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-#PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 
 # validate the input
 if [[ -z $1 ]]
@@ -26,11 +26,64 @@ handle_name() {
   [[ $1 =~ ^[A-Z][a-z]*$ ]]
 }
 
+# number search
+search_number() {
+
+  ELEMENT_SYMBOL="$($PSQL "SELECT symbol FROM elements WHERE atomic_number=$1")"
+  ELEMENT_NAME="$($PSQL "SELECT name FROM elements WHERE atomic_number=$1")"
+
+  ELEMENT_TYPE_NAME="$($PSQL "SELECT types.type FROM types INNER JOIN properties USING(type_id) WHERE atomic_number = $1")" 
+
+  ATOMIC_MASS="$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$1")"
+
+  MELTING_POINT="$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$1")"
+  BOLING_POINT="$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$1")"
+
+  echo "The element with atomic number $1 is $ELEMENT_NAME ($ELEMENT_SYMBOL). It's a $ELEMENT_TYPE_NAME, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOLING_POINT celsius."
+  
+}
+
+# symbol search
+search_symbol() {
+
+  ATOMIC_NUMBER="$($PSQL "SELECT atomic_number FROM elements WHERE symbol='$1'")"
+  ELEMENT_NAME="$($PSQL "SELECT name FROM elements WHERE atomic_number=$ATOMIC_NUMBER")"
+
+  ELEMENT_TYPE_NAME="$($PSQL "SELECT types.type FROM types INNER JOIN properties USING(type_id) WHERE atomic_number = $ATOMIC_NUMBER")" 
+
+  ATOMIC_MASS="$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$ATOMIC_NUMBER")"
+
+  MELTING_POINT="$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")"
+  BOLING_POINT="$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")"
+  
+  echo "The element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($1). It's a $ELEMENT_TYPE_NAME, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOLING_POINT celsius."
+  
+}
+
+# name search
+search_name() {
+
+  ATOMIC_NUMBER="$($PSQL "SELECT atomic_number FROM elements WHERE name='$1'")"
+  ELEMENT_NAME="$($PSQL "SELECT name FROM elements WHERE atomic_number=$ATOMIC_NUMBER")"
+
+  ELEMENT_TYPE_NAME="$($PSQL "SELECT types.type FROM types INNER JOIN properties USING(type_id) WHERE atomic_number = $ATOMIC_NUMBER")" 
+
+  ATOMIC_MASS="$($PSQL "SELECT atomic_mass FROM properties WHERE atomic_number=$ATOMIC_NUMBER")"
+
+  MELTING_POINT="$($PSQL "SELECT melting_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")"
+  BOLING_POINT="$($PSQL "SELECT boiling_point_celsius FROM properties WHERE atomic_number=$ATOMIC_NUMBER")"
+  
+  echo "The element with atomic number $ATOMIC_NUMBER is $ELEMENT_NAME ($1). It's a $ELEMENT_TYPE_NAME, with a mass of $ATOMIC_MASS amu. $ELEMENT_NAME has a melting point of $MELTING_POINT celsius and a boiling point of $BOLING_POINT celsius."
+  
+}
+
+
+
 # Determine the type of the input and call the function
 if is_number "$input"; then
-  echo "$input is a number"
+  search_number $input
 elif is_symbol "$input"; then
-  echo "$input is symbol"
+  search_symbol $input
 elif handle_name "$input"; then
-  echo "$input is an element name"
+  search_name $input
 fi
