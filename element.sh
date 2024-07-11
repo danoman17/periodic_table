b62dae21877a2a74c40ca094681fc0278c391ae6
Daniel Flores Rodríguez <daniflores374@gmail.com>
2024-07-11 20:46:37 +0000

    feat: read input

---
+++ element.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
+
+# TODO: validate the input
+
+input=$1
+
+echo "The input is: $input"
+
+# TODO: validate the type if the input <atomic_number> <element_symbol> <element_name>
+
