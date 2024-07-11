7ee6e00d332ead50bed9c7b125521dcc1e41b313
Daniel Flores Rodríguez <daniflores374@gmail.com>
2024-07-11 21:17:11 +0000

    feat: determine input type

--- element.sh
+++ element.sh
@@ -2,17 +2,35 @@
 
 #PSQL="psql --username=freecodecamp --dbname=<database_name> -t --no-align -c"
 
-# TODO: validate the input
-
+# validate the input
 if [[ -z $1 ]]
 then
   echo "Usage: $0 <atomic_number> | <symbol> | <name>"
   exit 1
 fi
 
+# assign the input 
 input=$1
 
-echo "The input is: $input"
+# Function to check if input is a number
+is_number() {
+  [[ $1 =~ ^[0-9]+$ ]]
+}
 
-# TODO: validate the type if the input <atomic_number> <element_symbol> <element_name>
+# Function to check if input is a single character (element symbol)
+is_symbol() {
+  [[ $1 =~ ^[A-Za-z]$ ]]
+}
 
+handle_name() {
+  [[ $1 =~ ^[A-Z][a-z]*$ ]]
+}
+
+# Determine the type of the input and call the function
+if is_number "$input"; then
+  echo "$input is a number"
+elif is_symbol "$input"; then
+  echo "$input is symbol"
+elif handle_name "$input"; then
+  echo "$input is an element name"
+fi
